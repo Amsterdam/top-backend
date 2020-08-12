f3e2b72e6f2a2d5657b6d686831a8969ef023b1c
Xavier Bloemen <xavier.bloemen@teamrockstars.nl>
2020-08-12 12:42:32 +0200

    isort

--- app/apps/cases/tests/tests_api.py
+++ app/apps/cases/tests/tests_api.py
@@ -14,7 +14,7 @@ from model_bakery import baker
 from rest_framework import status
 from rest_framework.test import APITestCase
 
-from utils.unittest_helpers import (
+from app.utils.unittest_helpers import (
     get_authenticated_client,
     get_unauthenticated_client,
 )
