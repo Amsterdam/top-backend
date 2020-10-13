6f2c8c1b2c25814a79b871b7e73fc8345083df9e
Xavier Bloemen <xavier.bloemen@teamrockstars.nl>
2020-10-13 12:18:33 +0200

    black issues fixed

--- app/apps/users/admin.py
+++ app/apps/users/admin.py
@@ -10,7 +10,10 @@ class UserAdmin(UserAdmin):
             "None",
             {"fields": ("email", "password", "username", "first_name", "last_name")},
         ),
-        ("None", {"fields": ("team_settings",)},),
+        (
+            "None",
+            {"fields": ("team_settings",)},
+        ),
         (
             "Permissions",
             {
