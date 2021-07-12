ba4a2a814c57e8959a22e7e462728004c86b637c
Maurice Guikema <mguikema@gmail.com>
2021-07-12 15:21:23 +0200

    auth for aza -> top connections

---
+++ app/apps/users/auth_apps.py
@@ -0,0 +1,24 @@
+from django.conf import settings
+from rest_framework.permissions import BasePermission
+
+
+class AppsKeyAuth(BasePermission):
+    SECRET_KEY = None
+
+    def has_permission(self, request, view):
+        try:
+            assert self.SECRET_KEY, "SECRET_KEY is not set"
+            print(request.META.get("HTTP_AUTHORIZATION", None))
+            print(self.SECRET_KEY)
+            secret_key_request = request.META.get("HTTP_AUTHORIZATION", None)
+            assert secret_key_request, "Secret key cannot be retrieved from request"
+
+            has_permission = secret_key_request == self.SECRET_KEY
+
+            return has_permission
+        except AssertionError:
+            return False
+
+
+class AZAKeyAuth(AppsKeyAuth):
+    SECRET_KEY = settings.SECRET_KEY_AZA_TOP
