c313a349206196ec157c4499f4e992f3734a6781
Maurice Guikema <mguikema@gmail.com>
2022-02-01 16:45:10 +0100

    feature: permits proxy to AZA

--- app/apps/permits/router.py
+++ app/apps/permits/router.py
@@ -1,5 +1,6 @@
-from apps.permits.views import DecosViewSet
+from apps.permits.views import DecosViewSet, PermitViewSet
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register(r"decos", DecosViewSet, basename="decos")
+router.register(r"addresses", PermitViewSet, basename="addresses")
