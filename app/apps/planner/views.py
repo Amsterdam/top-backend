2bf2cb425b06fb55f2606fe44a916a4bc30a5425
Nino van der Laan <n.van.der.laan@amsterdam.nl>
2024-02-29 09:41:58 +0100

    more fields

--- app/apps/planner/views.py
+++ app/apps/planner/views.py
@@ -34,7 +34,7 @@ class TeamSettingsViewSet(ModelViewSet):
     """
 
     serializer_class = TeamSettingsSerializer
-    queryset = TeamSettings.objects.filter(enabled=True).values('name', 'id')
+    queryset = TeamSettings.objects.filter(enabled=True).values('name', 'id', 'zaken_team_id', 'fraud_prediction_model')
 
     @extend_schema(
         description="Gets the reasons associated with the requested team",
