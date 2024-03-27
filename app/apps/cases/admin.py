bae87776a5f9717df6c6c039b07bf187342abaf1
Nino van der Laan <n.van.der.laan@amsterdam.nl>
2024-03-27 14:06:57 +0100

    try view for redis keys

--- app/apps/cases/admin.py
+++ app/apps/cases/admin.py
@@ -1,7 +1,10 @@
 from apps.cases.models import Case
 from django.contrib import admin
 from django.contrib.admin import SimpleListFilter
-
+from django.contrib import admin
+from django.http import HttpResponse
+from django.urls import path
+from django_redis import get_redis_connection
 
 class HasUnderscoreFilter(SimpleListFilter):
     title = "underscore"
@@ -26,3 +29,21 @@ class CaseAdmin(admin.ModelAdmin):
     list_filter = (HasUnderscoreFilter, "is_top_bwv_case")
     list_display = ("id", "case_id", "is_top_bwv_case")
     search_fields = ("case_id",)
+
+
+class RedisKeyAdmin(admin.ModelAdmin):
+    def get_urls(self):
+        urls = super().get_urls()
+        custom_urls = [
+            path('redis-keys/', self.show_redis_keys),
+        ]
+        return custom_urls + urls
+
+    def show_redis_keys(self, request):
+        redis_connection = get_redis_connection()
+        keys = redis_connection.keys('*')  # Change the pattern according to your needs
+        key_list = [key.decode('utf-8') for key in keys]
+        content = '\n'.join(key_list)
+        return HttpResponse(content, content_type='text/plain')
+
+admin.site.register(RedisKeyAdmin)
