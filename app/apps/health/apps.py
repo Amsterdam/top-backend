053aa675e9e594982607935eab29e1433ed3e8e3
Nino van der Laan <n.van.der.laan@amsterdam.nl>
2024-03-27 14:49:53 +0100

    register the check

--- app/apps/health/apps.py
+++ app/apps/health/apps.py
@@ -10,10 +10,12 @@ class HealthConfig(AppConfig):
             BAGServiceCheck,
             BRKServiceCheck,
             CeleryExecuteTask,
+            CeleryStuckTasks
         )
 
         plugin_dir.register(BAGServiceCheck)
         plugin_dir.register(BRKServiceCheck)
         plugin_dir.register(CeleryExecuteTask)
+        plugin_dir.register(CeleryStuckTasks)
         # Only uncomment below if endpoints for OnderhuurHitkans are available on production
         # plugin_dir.register(OnderhuurHitkansServiceCheck)
