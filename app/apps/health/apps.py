31f8b86cfa56512b6f997bdee6ad219d3b68310c
Nino van der Laan <n.van.der.laan@amsterdam.nl>
2024-03-28 13:41:40 +0100

    remove check

--- app/apps/health/apps.py
+++ app/apps/health/apps.py
@@ -13,9 +13,9 @@ class HealthConfig(AppConfig):
             CeleryStuckTasks
         )
 
-        plugin_dir.register(BAGServiceCheck)
-        plugin_dir.register(BRKServiceCheck)
-        plugin_dir.register(CeleryExecuteTask)
+        # plugin_dir.register(BAGServiceCheck)
+        # plugin_dir.register(BRKServiceCheck)
+        # plugin_dir.register(CeleryExecuteTask)
         plugin_dir.register(CeleryStuckTasks)
         # Only uncomment below if endpoints for OnderhuurHitkans are available on production
         # plugin_dir.register(OnderhuurHitkansServiceCheck)
