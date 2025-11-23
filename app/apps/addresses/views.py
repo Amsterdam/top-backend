f37b235aae1fa046fffd96609b36a4be8b9fa3a9
Remy van der Wereld <r.van.der.wereld@amsterdam.nl>
2025-11-23 14:39:24 +0100

    fix(addresses): change fetch_residents method from POST to GET

--- app/apps/addresses/views.py
+++ app/apps/addresses/views.py
@@ -73,7 +73,7 @@ def fetch_registrations(bag_id, auth_header=None, query_params=None):
 
 def fetch_residents(bag_id, body, auth_header=None):
     url = f"{settings.ZAKEN_API_URL}/addresses/{bag_id}/residents-new/"
-    response = requests.post(
+    response = requests.get(
         url, timeout=30, headers=get_headers(auth_header), json=body
     )
 
